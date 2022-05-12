00401700e5278b5f00f4c85b3d1cb4667d318736
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2022-05-12 09:50:01 +0300

    added requirements.txt

--- book/urls.py
+++ book/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('books', views.BookList.as_view()),
     path('books/<int:pk>', views.BookDetail.as_view()),
+    path('authors', views.AuthorList.as_view()),
+    path('authors/<int:pk>', views.AuthorDetail.as_view()),
 ]
