d28f17cabf9d4bbfe3ba4cae95299c38934efa02
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2022-05-13 09:31:00 +0300

    set up unit test

--- book/tests.py
+++ book/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
-
+from django.contrib.auth.models import User
+from .models import *
 # Create your tests here.
+
+
+class modelTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super(ModelTest, cls).setUpClass()
+
+        booksample = Book(title='The World of geography', Author=1, year=2013, description='sample', quantity='2')
+        booksample.save()
