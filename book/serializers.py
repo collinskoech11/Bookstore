7d76dc48afbae3574433e8b9e6e60fed111d3832
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2022-05-14 09:17:03 +0300

    added status to Book model

--- book/serializers.py
+++ book/serializers.py
@@ -1,10 +1,29 @@
+from re import I
 from .models import *
 from rest_framework import serializers
+status = "bad"
 
 class BookSerializer(serializers.ModelSerializer):
+    book_status = serializers.SerializerMethodField('_get_book_status')
+
+    def _get_book_status(self, book_object):
+        global status
+        quantity = getattr(book_object, 'quantity')
+        if quantity > 10:
+            status = "good"
+            return status
+        elif quantity > 4 and quantity < 10:
+            status = "Bad"
+            return status
+        elif quantity > 0 and quantity < 5:
+            status = "Critical"
+            return status
+        elif quantity == 0:
+            status = "Out of stock"
+            return status
     class Meta:
         model = Book
-        fields = ['id','title','Author','year','description','quantity']
+        fields = ['id','title','Author','year','description','quantity','book_status']
 
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
