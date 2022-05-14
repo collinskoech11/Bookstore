3bf01d7e1ca8d8794b554ee6d42504aa6f19f537
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2022-05-14 21:00:20 +0300

    added test data

---
+++ data.py
@@ -0,0 +1,86 @@
+[
+    {
+        "title": "Harley Quinn & the Birds of Prey: The Hunt for Harley",
+        "Author": 2,
+        "year": 2000,
+        "description": "nature book",
+        "quantity": 6
+    },
+    {
+        "title": "Jane Boleyn: The True Story of the Infamous Lady Rochford",
+        "Author": 3,
+        "year": 2000,
+        "description": "science fiction",
+        "quantity": 1
+    },
+    {
+        "title": "Cherrypickers' Guide to Rare Die Varieties of United States Coins: 1 (An Official Whitman Guidebook)",
+        "Author": 4,
+        "year": 2010,
+        "description": "self help book",
+        "quantity": 6
+    },
+    {
+        "title": "Cherrypickers' Guide to Rare Die Varieties of United States Coins: 2 (An Official Whitman Guidebook)",
+        "Author": 4,
+        "year": 2012,
+        "description": "science book",
+        "quantity": 9
+    },
+    {
+        "title": "The Liberal Redneck Manifesto: Draggin' Dixie Outta the Dark (Hardcover)",
+        "Author": 5,
+        "year": 2012,
+        "description": "science fiction",
+        "quantity": 1
+    },
+    {
+        "title": "Cleopatra: A Life (Hardcover)",
+        "Author": 6,
+        "year": 2010,
+        "description": "self help book",
+        "quantity": 6
+    },
+    {
+        "title": "Ancillary Justice (Imperial Radch, #1)by Ann Leckie ",
+        "Author": 7,
+        "year": 2012,
+        "description": "science book",
+        "quantity": 9
+    },
+    {
+        "title": "Clockwork Angel (The Infernal Devices, #1)",
+        "Author": 8,
+        "year": 2012,
+        "description": "science fiction",
+        "quantity": 1
+    },
+    {
+        "title": "Missoula: Rape and the Justice System in a College Town (Hardcover)",
+        "Author": 9,
+        "year": 2010,
+        "description": "self help book",
+        "quantity": 6
+    },
+    {
+        "title": "Wither (The Chemical Garden, #1)",
+        "Author": 10,
+        "year": 2012,
+        "description": "science book",
+        "quantity": 9
+    },
+    {
+        "title": "Fever (The Chemical Garden, #2)",
+        "Author": 10,
+        "year": 2012,
+        "description": "science fiction",
+        "quantity": 1
+    },
+    {
+        "title": "Delirium (Delirium, #1)",
+        "Author": 10,
+        "year": 2010,
+        "description": "self help book",
+        "quantity": 6
+    }
+]
