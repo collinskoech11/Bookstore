847fdb9352877956b40df6c20304acf7c5d7f754
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2022-05-14 22:47:46 +0300

    update migrations

--- bookstore/tests.py
+++ bookstore/tests.py
@@ -6,14 +6,13 @@ class ModelTest(TestCase):
     def setUpClass(cls):
         # below code will fix AttributeError: type object 'Model Test' has no attribute 'cls_atomics' error.
         super(ModelTest, cls).setUpClass()
-
+        # test Book Model
         # create a Author model object in temporary database.
         sampleauthor = Author(fname='tom', lname='onyango', email="tom@gmail.com", Date_of_birth='2000-12-12')
         sampleauthor.save()
         # get author .
         sampleuthor = Author.objects.get(fname='tom')
-        print('Added author data : ')
-        print(sampleauthor)
+        print('Added author data : ', sampleauthor)
         # create and save the Author object.
     def testAuthorModel(self):
         sampleauthor = Author.objects.get(lname='onyango')
